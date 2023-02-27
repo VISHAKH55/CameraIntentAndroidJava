0a87cae1d0c776748abc670784fdd6e64f6ae807
VISHAKH55 <126440629+VISHAKH55@users.noreply.github.com>
2023-02-27 11:07:34 +0530

    Initial commit Vishakh 27_02_2023_11_07

---
+++ app/src/main/java/com/example/cameraintent/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.cameraintent;
+
+import androidx.activity.result.ActivityResult;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int pic_id=123;
+    Button Camera_open_id;
+    ImageView Click_image_id;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Camera_open_id=findViewById(R.id.btn1);
+        Click_image_id=findViewById(R.id.image);
+        Camera_open_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent,pic_id);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==pic_id){
+            Bitmap photo=(Bitmap) data.getExtras().get("data");
+            Click_image_id.setImageBitmap(photo);
+        }
+    }
+}
+
