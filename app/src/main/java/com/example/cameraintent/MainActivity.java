521dbe2731ea540df72818431ad35b52195da729
VISHAKH55 <126440629+VISHAKH55@users.noreply.github.com>
2023-02-27 11:11:02 +0530

    scond commit vishakh 27_02_2023_11_10

--- app/src/main/java/com/example/cameraintent/MainActivity.java
+++ app/src/main/java/com/example/cameraintent/MainActivity.java
@@ -18,32 +18,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int pic_id=123;
+    private static final int pic_id = 123;
     Button Camera_open_id;
     ImageView Click_image_id;
 
-
+    TextView name;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Camera_open_id=findViewById(R.id.btn1);
-        Click_image_id=findViewById(R.id.image);
+        Camera_open_id = findViewById(R.id.btn1);
+        Click_image_id = findViewById(R.id.image);
         Camera_open_id.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent camera_intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(camera_intent,pic_id);
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent, pic_id);
             }
         });
-
 
 
     }
@@ -51,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==pic_id){
-            Bitmap photo=(Bitmap) data.getExtras().get("data");
+        if (requestCode == pic_id) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
             Click_image_id.setImageBitmap(photo);
         }
     }
